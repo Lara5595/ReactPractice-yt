d5f5378d0cbf0eab19022e8df1771deea30163a8
Lara5595 <108631647+Lara5595@users.noreply.github.com>
2023-02-09 18:18:15 -0600

    on module 8 and fix api

--- src/ExcuserApi.js
+++ src/ExcuserApi.js
@@ -2,13 +2,15 @@ import "./App.css";
 import Axios from "axios";
 import { useEffect, useState } from "react";
 
-// https://excuser.herokuapp.com/v1/excuse/  api does not work anymore
+// https://excuser-three.vercel.app/v1/excuse/
+
+
 
 export const ExcuserApi = () => {
 const [generatedExcuse, setGeneratedExcuse] = useState("");
 
 const fetchExcuse = (excuse) => {
-    Axios.get(`https://excuser.herokuapp.com/v1/excuse/${excuse}/`).then((res) => {
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then((res) => {
         setGeneratedExcuse(res.data[0].excuse)
         console.log(res.data);
     })
