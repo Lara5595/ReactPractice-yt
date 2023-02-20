423a9ae3d36ca1d636e221fe93b6e1aa4af2fb74
David Lara <david.a.lara05@gmail.com>
2023-02-19 20:54:06 -0600

    stayed on getting data from forms

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,14 +1,23 @@
+import { useForm } from 'react-hook-form'
+
 export const Form = () => {
+    const {register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
     return (
         <div>
        <h1>Hello this are forms </h1>
 
-       <form>
-        <input type= "text" placeholder="Full Name..." />
-        <input type= "text" placeholder="Email..." />
-        <input type= "text" placeholder="Age..." />
-        <input type= "password" placeholder="Pasword..." />
-        <input type= "password" placeholder="Confirm Password..." />
+       <form onSubmit={handleSubmit(onSubmit)}>
+
+        <input type= "text" placeholder="Full Name..."   {...register("fullName")} />
+        <input type= "text" placeholder="Email..."      {...register("email")} />
+        <input type= "text" placeholder="Age..."         {...register("age")} />
+        <input type= "password" placeholder="Pasword..."        {...register("password")}  />
+        <input type= "password" placeholder="Confirm Password..."       {...register("confirmPassword")} />
         <input type= "submit" />
 
 
